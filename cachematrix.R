7ec9ba40d16926c1c8b50cb23c7e59737e33ed87
mdhwn21 <mohit.nkdhawan@gmail.com>
2015-10-23 21:21:53 -0400

    Inital Checkin
    inital  checkin for the  course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Modeling the inverse function affter the example in the course for mean 
+## make cache matrix makes a cache 
 
-## Write a short comment describing this function
+## Makes a special matric which caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	m<-NULL
+	set<-function(y){
+		x <<- y
+            m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) m <<- inverse
+      getinverse <- function() m
+      list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## this function either computes the inverse or get the cached 
+## version of the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	  m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
